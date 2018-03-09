f6eb2e3856f805203a16d055d78114101bfa97b7
whiskyhou <ghostgaz@126.com>
2018-03-09 11:18:53 +0800

    2018/3/9 测试鼠标事件

--- out/1.js
+++ out/1.js
@@ -8,14 +8,15 @@
 //     else
 //         document.body.innerText = "K.O."
 // }, 1000);
-var canvas = document.getElementById("gameCanvas");
-var context = canvas.getContext("2d");
 /**
  * Main-Loop
  */
+var canvas = document.getElementById("gameCanvas");
+var context = canvas.getContext("2d");
 var y = 0;
+var x = 0;
 var image = new Image();
-image.src = 'logo_640.jpg';
+image.src = './assets/logo_640.jpg';
 function enterFrame() {
     if (!context)
         return;
@@ -23,13 +24,23 @@ function enterFrame() {
     canvas.width = 400;
     canvas.height = 600;
     context.drawImage(image, 0, 0);
-    context.rect(0, y, 100, 100);
+    context.rect(x, y, 100, 100);
     context.fillStyle = 'red';
     context.fill();
     context.fillStyle = 'black';
     context.fillText("hello houyi", 0, 100);
     requestAnimationFrame(enterFrame);
 }
-var image = new Image();
-image.src = 'logo_640.jpg';
+window.onclick = function () {
+    x = getMousePosX(event);
+    y = getMousePosY(event);
+};
+function getMousePosX(event) {
+    var e = event || window.event;
+    return e.clientX;
+}
+function getMousePosY(event) {
+    var e = event || window.event;
+    return e.clientY;
+}
 requestAnimationFrame(enterFrame);
