f35abd7e1b61a18808eff4cc415d1d7c2ec0e789
houyi <ghostgaz@126.com>
2018-03-10 14:04:07 +0800

    2018/3/10 测试控制飞机和开火

--- out/1.js
+++ out/1.js
@@ -13,56 +13,99 @@
  */
 var canvas = document.getElementById("gameCanvas");
 var context = canvas.getContext("2d");
-var y = 0;
-var x = 0;
-var image = new Image();
-image.src = './assets/logo_640.jpg';
+var player_x = 0;
+var player_y = 0;
+var player = new Image();
+player.src = './assets/player_60.png';
+var bg = new Image();
+bg.src = './assets/bg_400_600.jpg';
+// var image = new Image();
+// image.src = './assets/logo_640.jpg';
 function enterFrame() {
     if (!context)
         return;
-    y++;
+    // y++;
     // x = getMousePosX(event);
     // y = getMousePosY(event);
     canvas.width = 400;
     canvas.height = 600;
-    context.drawImage(image, 0, 0);
-    context.rect(x, y, 100, 100);
-    context.fillStyle = 'red';
-    context.fill();
-    context.fillStyle = 'black';
-    context.fillText("hello houyi", 0, 100);
+    // context.drawImage(image, 0, 0);
+    // context.rect(x, y, 100, 100);
+    // context.fillStyle = 'red';
+    // context.fill();
+    // context.fillStyle = 'black';
+    // context.fillText("hello houyi", 0, 100);
+    context.drawImage(bg, 0, 0);
+    context.drawImage(player, player_x, player_y);
     requestAnimationFrame(enterFrame);
 }
-// setInterval(function () {
-//     x = getMousePosX(event);
-//     y = getMousePosY(event);
-// }, 10);
-window.onmousemove = function () {
-    var e = this.event || this.window.event;
-    // x = getMousePosX(e)
-    var mousePos = mousePosition(e);
-    x = mousePos.x;
-    y = mousePos.y;
-};
-window.onclick = function () {
-    x = getMousePosX(event);
-    y = getMousePosY(event);
-};
+window.onmousemove = setPlayerPosAsMousePos;
+window.onclick = text;
+function setPlayerPosAsMousePos(event) {
+    var event = event || window.event;
+    var mousePos = mousePosition(event);
+    var x = mousePos.x - 30;
+    var y = mousePos.y - 30;
+    if (x >= 0 && x <= 340)
+        player_x = x;
+    if (y >= 0 && y <= 540)
+        player_y = y;
+}
 function mousePosition(ev) {
-    if (ev.pageX || ev.pageY) {
-        return { x: ev.pageX, y: ev.pageY };
+    return { x: ev.clientX, y: ev.clientY };
+}
+/**
+ * 鼠标点击事件 —— 玩家开火 方法集
+ *
+ * 存在问题 —— 只能一发一发打，上一发子弹如果未消失再次开火，上一发直接消失并且下一发的飞行速度会将上一发叠加
+ */
+// var list: PlayerBullet[];
+function text(ev) {
+    // var bullet = new PlayerBullet(player_x, player_y);
+    // list.push(bullet);
+    fireNormal(player_x, player_y);
+}
+var fireX;
+var fireY;
+function fireNormal(x, y) {
+    fireX = x + 30;
+    fireY = y;
+    requestAnimationFrame(fireNormalFrame);
+}
+function fireNormalFrame() {
+    if (!context)
+        return;
+    fireY -= 5;
+    context.rect(fireX, fireY, 4, 20);
+    context.fillStyle = 'red';
+    context.fill();
+    if (fireY < -20)
+        return;
+    requestAnimationFrame(fireNormalFrame);
+}
+/**
+ * 玩家子弹 类
+ *
+ * 测试失败 —— 通过 requestAnimationFrame() 访问的成员函数 无法调用对象属性值
+ */
+var PlayerBullet = /** @class */ (function () {
+    function PlayerBullet(px, py) {
+        this.x = px + 28;
+        this.y = py + 10;
     }
-    return {
-        x: ev.clientX + document.body.scrollLeft - document.body.clientLeft,
-        y: ev.clientY + document.body.scrollTop - document.body.clientTop
-    };
-}
-function getMousePosX(event) {
-    var e = event || window.event;
-    return e.clientX;
-}
-function getMousePosY(event) {
-    var e = event || window.event;
-    return e.clientY;
-}
+    return PlayerBullet;
+}());
+/**
+ * 敌人飞机 类
+ *
+ * 尚未编写
+ */
+var Enemy = /** @class */ (function () {
+    function Enemy() {
+        this.x = 0;
+        this.y = 0;
+        this.hp = 5;
+    }
+    return Enemy;
+}());
 requestAnimationFrame(enterFrame);
--- src/1.ts
+++ src/1.ts
@@ -14,64 +14,132 @@
  */
 var canvas = document.getElementById("gameCanvas") as HTMLCanvasElement;
 var context = canvas.getContext("2d");
-var y = 0;
-var x = 0;
-var image = new Image();
-image.src = './assets/logo_640.jpg';
+var player_x = 0;
+var player_y = 0;
+var player = new Image();
+player.src = './assets/player_60.png';
+var bg = new Image();
+bg.src = './assets/bg_400_600.jpg';
+// var image = new Image();
+// image.src = './assets/logo_640.jpg';
 
 function enterFrame() {
     if (!context)
         return;
-    y++;
+    // y++;
     // x = getMousePosX(event);
     // y = getMousePosY(event);
     canvas.width = 400;
     canvas.height = 600;
-    context.drawImage(image, 0, 0);
-    context.rect(x, y, 100, 100);
-    context.fillStyle = 'red';
-    context.fill();
-    context.fillStyle = 'black';
-    context.fillText("hello houyi", 0, 100);
+    // context.drawImage(image, 0, 0);
+    // context.rect(x, y, 100, 100);
+    // context.fillStyle = 'red';
+    // context.fill();
+    // context.fillStyle = 'black';
+    // context.fillText("hello houyi", 0, 100);
+    context.drawImage(bg, 0, 0);
+    context.drawImage(player, player_x, player_y);
 
     requestAnimationFrame(enterFrame);
 }
 
-// setInterval(function () {
-//     x = getMousePosX(event);
-//     y = getMousePosY(event);
-// }, 10);
+window.onmousemove = setPlayerPosAsMousePos;
 
-window.onmousemove = function () {
-    var e = this.event || this.window.event;
-    // x = getMousePosX(e)
-    var mousePos = mousePosition(e);
-    x = mousePos.x;
-    y = mousePos.y;
-}
+window.onclick = text;
 
-window.onclick = function () {
-    x = getMousePosX(event);
-    y = getMousePosY(event);
+function setPlayerPosAsMousePos(event: any) {
+    var event = event || window.event;
+    var mousePos = mousePosition(event);
+    var x = mousePos.x - 30;
+    var y = mousePos.y - 30;
+    if (x >= 0 && x <= 340)
+        player_x = x;
+    if (y >= 0 && y <= 540)
+        player_y = y;
 }
 
 function mousePosition(ev: any) {
-    if (ev.pageX || ev.pageY) {//firefox、chrome等浏览器
-        return { x: ev.pageX, y: ev.pageY };
-    }
-    return {// IE浏览器
-        x: ev.clientX + document.body.scrollLeft - document.body.clientLeft,
-        y: ev.clientY + document.body.scrollTop - document.body.clientTop
-    };
+    return { x: ev.clientX, y: ev.clientY };
 }
 
-function getMousePosX(event: any) {
-    var e = event || window.event;
-    return e.clientX;
+/**
+ * 鼠标点击事件 —— 玩家开火 方法集
+ * 
+ * 存在问题 —— 只能一发一发打，上一发子弹如果未消失再次开火，上一发直接消失并且下一发的飞行速度会将上一发叠加
+ */
+// var list: PlayerBullet[];
+function text(ev: any) {
+    // var bullet = new PlayerBullet(player_x, player_y);
+    // list.push(bullet);
+    fireNormal(player_x, player_y);
 }
-function getMousePosY(event: any) {
-    var e = event || window.event;
-    return e.clientY;
+var fireX: number;
+var fireY: number;
+function fireNormal(x: number, y: number) {
+    fireX = x + 30;
+    fireY = y;
+    requestAnimationFrame(fireNormalFrame);
+}
+function fireNormalFrame() {
+    if (!context)
+        return;
+    fireY -= 5;
+    context.rect(fireX, fireY, 4, 20);
+    context.fillStyle = 'red';
+    context.fill();
+    if (fireY < -20)
+        return;
+    requestAnimationFrame(fireNormalFrame);
+}
+
+
+/**
+ * 玩家子弹 类
+ * 
+ * 测试失败 —— 通过 requestAnimationFrame() 访问的成员函数 无法调用对象属性值
+ */
+class PlayerBullet {
+    x: number;
+    y: number;
+    constructor(px: number, py: number) {
+        this.x = px + 28;
+        this.y = py + 10;
+    }
+    // fire() {
+    //     console.log(this.y);
+    //     console.log(this.x);
+    //     requestAnimationFrame(this.fireUpdate);
+    // }
+    // fireUpdate() {
+    //     if (!context)
+    //         return;
+    //     // this.y -= 10;
+    //     console.log(this.y);
+    //     console.log(this.x);
+    //     context.rect(this.x, this.y, 4, 20);
+    //     context.fillStyle = 'red';
+    //     context.fill();
+    //     if (this.y < -20)
+    //         return;
+    //     requestAnimationFrame(this.fireUpdate);
+    // }
+}
+
+
+/**
+ * 敌人飞机 类
+ * 
+ * 尚未编写
+ */
+class Enemy {
+    x: number;
+    y: number;
+    hp: number;
+    constructor() {
+        this.x = 0;
+        this.y = 0;
+        this.hp = 5;
+    }
 }
 
 requestAnimationFrame(enterFrame);
