fe5df67211d2c4b7fe5a664ed2eab4e08d4873ef
whiskyHou_macbookPro <ghostgaz@126.com>
2018-03-28 21:56:49 +0800

    2018/3/28 create homework4

--- src/game.ts
+++ src/game.ts
@@ -295,6 +295,88 @@ class GameMap extends DisplayObjectContainer {
 }
 
 
+/**
+ * 走路命令
+ */
+class WalkCommand extends Command {
+    fromX: number;
+    fromY: number;
+    toX: number;
+    toY: number;
+
+    constructor(fromX: number, fromY: number, toX: number, toY: number) {
+        super();
+        this.fromX = fromX;
+        this.fromY = fromY;
+        this.toX = toX;
+        this.toY = toY;
+    }
+
+    execute(callback: Function): void {
+        console.log(`开始走路！！！从(${this.fromX}, ${this.fromY})出发`);
+
+        map.grid.setStartNode(this.fromX, this.fromY);
+        map.grid.setEndNode(this.toX, this.toY);
+        const findpath = new astar.AStar();
+        findpath.setHeurisitic(findpath.diagonal);
+        const result = findpath.findPath(map.grid);
+        // console.log(map.grid.toString());
+        console.log(findpath._path);
+
+        let path;
+        if (result) {
+            path = findpath._path;
+            path.shift();
+            this.walk(path, callback);
+        } else {
+            player.moveStatus = true;
+            callback();
+        }
+
+        // setTimeout(() => {
+        //     console.log(`到达目标(${this.toX}, ${this.toY})`);
+        //     callback();
+        // }, 3000)
+    }
+
+    walk(path: astar.Node[], callback: Function) {
+        setTimeout(() => {
+            let node = path.shift();
+            if (node) {
+                player.dispatchEvent({ message: 'walkOneStep', nodeX: node.x, nodeY: node.y });
+            }
+            else {
+                console.log(`到达地点！！！(${this.toX},${this.toY})`);
+                player.moveStatus = true;
+                callback();
+                return;
+            }
+            this.walk(path, callback);
+        }, PLAYER_WALK_SPEED);
+    }
+}
+
+
+/**
+ * 拾取命令
+ */
+class PickCommand extends Command {
+    equipment: Equipment;
+
+    constructor(equipment: Equipment) {
+        super();
+        this.equipment = equipment;
+    }
+
+    execute(callback: Function): void {
+        player.pick(this.equipment);
+        console.log(`捡起了${this.equipment.toString()}`);
+        map.dispatchEvent({ message: 'pickEquipment' });
+        callback();
+    }
+}
+
+
 
 
 // 鼠标点击事件，捕获所有被点击到的 DisplayObject，并从叶子节点依次向上通知它们的监听器，监听器执行
