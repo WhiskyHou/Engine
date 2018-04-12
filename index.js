34e2b5238878ae3b6224689d94c693dfee348dda
whiskyHou_acerSwift <ghostgaz@126.com>
2018-04-12 19:29:47 +0800

    调整文件结构

--- index.js
+++ index.js
@@ -4,14 +4,15 @@ const url = require('url')
 
 function createWindow() {
     // Create the browser window.
-    win = new BrowserWindow({ width: 800, height: 600 })
+    win = new BrowserWindow({ width: 1600, height: 1000 })
 
     // 然后加载应用的 index.html。
     win.loadURL(url.format({
-        pathname: path.join(__dirname, 'index.html'),
+        pathname: path.join(__dirname, 'editor/index.html'),
         protocol: 'file:',
         slashes: true
     }))
+    win.openDevTools()
 }
 
 app.on('ready', createWindow)
